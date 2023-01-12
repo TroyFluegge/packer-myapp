d7db283042f07db24b741ed0aefa83cbffc24d8b
Troy Fluegge <troy@hashicorp.com>
2023-01-11 20:15:10 -0600

    Updated App

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ func main() {
 	log.Print("Starting up!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello world! v7.5")
+		fmt.Fprint(w, "Hello world! v7.6")
 	})
 
 	// a truly basic health endpoint
