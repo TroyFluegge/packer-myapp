14289b1e6d8436458fe5aae80383c2fdb5eb5c63
Troy Fluegge <troy@hashicorp.com>
2024-03-12 14:42:51 -0500

    Update setup.sh
    Testing

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-##sudo apt-get update
-##sudo apt-get -y upgrade
+#sudo apt-get update
+#sudo apt-get -y upgrade
 
 # Add OS hardening configurations, security agents, 
 # and other base image settings here.
