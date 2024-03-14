ca6102c911cd62f3b373e18590c885d11e19cd0c
Troy Fluegge <troy@hashicorp.com>
2024-03-14 08:00:56 -0500

    Update main.go to 2.5

--- app/main.go
+++ app/main.go
@@ -10,12 +10,12 @@ func main() {
 	log.Print("Starting up!")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello world! v2.4")
+		fmt.Fprint(w, "Hello world! v2.5")
 	})
 
 	// a truly basic health endpoint
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "looks good v2.4")
+		fmt.Fprint(w, "looks good v2.5")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
