42ce9b3d5abdbc8560c5ad2eca3fbc6944cdcf41
Troy Fluegge <troy@hashicorp.com>
2024-03-13 21:08:09 -0500

    Patching Ubuntu 23.04 - Base Image
    Updating to 23.04 Ubuntu

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -5,5 +5,5 @@ sudo apt-get install -y -q
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Add OS hardening configurations, security agents, 
+## Add OS hardening configurations, security agents, 
 # and other base image settings here.
